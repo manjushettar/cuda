1ff3cb23f0242b94c1a85d460e9860f626423163
Manju <manjshettar@gmail.com>
2024-09-12 20:41:35 -0700

    push

--- basic/matrixMul.cu
+++ basic/matrixMul.cu
@@ -2,97 +2,166 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define N 1024
-__global__ void matrixMul(int *a, int *b, int *c){
-	int row = threadIdx.x + blockDim.x*blockIdx.x;
-	int col = threadIdx.y + blockDim.y*blockIdx.y;
+#define SHARED_SIZE 16 * 16
+__global__ void gemm_v0(size_t m, size_t n, size_t k,
+			const float *A, const float *B, float *C, const float alpha){
+	
+	// A = m x k
+	// B = k x n	
+	// C = m x n
 
-	if(row < N && col < N){
-		int dotProd = 0;
-		for(int i = 0; i < N; i++){
-			dotProd += a[row * N + i] * b[i * N + col];
+	// k = stride for accessing elements in row for A since A is m x k
+	// n = stride for accessing elements in col for B since B is k x n
+	// n = stride for accessing elements for C since C is m x n
+
+	int col = threadIdx.x + blockDim.x * blockIdx.x;
+	int row = threadIdx.y + blockDim.y * blockIdx.y;
+
+	if(row < m && col < n){
+		float dotProd = 0.0;
+		for(int i = 0; i < k; i++){
+			dotProd += A[row * k + i] * B[i * n + col];
 		}
-		c[row * N + col] = dotProd;
+		C[row * n + col] = alpha * dotProd;
 	}
 }
 
-void fill_matrices(int *a, int *b){
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < N; j++){
-			a[i * N + j] = i;
-			b[i * N + j] = i;
+__global__ void gemm_v1_global(size_t m, size_t n, size_t k,
+				const float *A, const float *B, float *C, const float alpha){
+	// wtf?	
+	int col = threadIdx.x + blockDim.x * blockIdx.x;
+	int row = threadIdx.y + blockDim.y * blockIdx.y;
+
+	if(row < m && col < n){
+		float dotProd = 0.0;
+		for(int i = 0; i < k; i++){
+			dotProd += A[row * k + i] * B[i * n + col];
+		}
+		C[row * n + col] = alpha * dotProd;
+	}
+
+}
+
+__global__ void gemm_shared(size_t m, size_t n, size_t k, size_t tile_width, const float *A, const float *B, float *C, const float alpha){
+	int ty = threadIdx.y;
+	int tx = threadIdx.x;
+
+	int row = ty + blockIdx.y * blockDim.y;
+	int col = tx + blockIdx.x * blockDim.x;
+
+	__shared__ float A_SHARED[SHARED_SIZE];
+	__shared__ float B_SHARED[SHARED_SIZE];
+	
+	float Cvalue = 0.f;
+	for(int t = 0; t < k / tile_width; t++){
+		// load into shared
+		A_SHARED[ty * tile_width + tx] = A[(k * row + t * tile_width + tx)];
+		B_SHARED[ty * tile_width + tx] = B[(t * tile_width + ty) * n + col];
+
+		// sync
+		__syncthreads();
+		
+			
+		for(int i = 0; i < tile_width; i++){
+			Cvalue += A_SHARED[tile_width * ty + i] * B_SHARED[i * tile_width + tx];
+		}
+		__syncthreads();
+	}
+	C[row * n + col] = Cvalue * alpha;
+	
+}
+
+void fill_matrix(float *a, int m, int n){
+	for(int row = 0; row < m; row++){
+		for(int col = 0; col < n; col++){
+			a[row * n + col] = 2.0;
 		}
 	}
 }
 
-void check_errors(int *a, int *b, int *c){
-	int *temp;
-	temp = (int *)malloc(N * N * sizeof(int));	
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < N; j++){
-			for(int k = 0; k < N; k++){
-				temp[i * N + j]+= a[i * N + k] * b[k * N + j];
+void check_errors(int m, int n, int k, float alpha,
+		  float *a, float *b, float *c){
+	float *temp;
+	temp = (float *)malloc(m * n * sizeof(float));	
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < n; j++){
+			temp[i * n + j] = 0.0;
+			for(int off = 0; off < k; off++){
+				temp[i * n + j]+= alpha * a[i * k + off] * b[off * n + j];
 			}
 		}
 	}
 
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < N; j++){
-			assert(temp[i * N + j] == c[i * N + j]);
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < n; j++){
+			assert(temp[i * n + j] == c[i * n + j]);
 		}
 	}
+	printf("Succesfully completed BatChest!\n");
+	free(temp);
 }
 
 int main(){
-	// 2x2 thread blocks
-	// x and y dimensions -> x,y dimension of thread (row, col)
-	// each thread writes to an index on the output matrix
+	size_t m, k, n;
+	m = 2048;
+	k = 2048;
+	n = 2048;
+	// A = m * k	
+	float *ha, *da;
+	size_t a_size = m * k * sizeof(float);
 
-	// coalescing writes: matrices are represented as arrays
-	// a00 | a01 | a10 | a11 (2x2 matrix)
-	// if we do column after column loading: we end up having staggered access patterns
-	// if we do row after row loading: all of our loads are sequential (no need to calculate offsets)
-	// might as well represent our own matrices as N * N arrays
-	// then... we must calculate for offset row * N + col
+	ha = (float *)malloc(a_size);
+	cudaMalloc(&da, a_size);
+	
+	// B = k * n
+	float *hb, *db;
+	size_t b_size = k * n * sizeof(float);
 
-	// in matrix mul:
-	// we load all the first elements of the rows (all located in one straight line)
-	// then we load the first elements of the column (diverse accesses + offsets)
+	hb = (float *)malloc(b_size);
+	cudaMalloc(&db, b_size);
 
-	size_t size = N * N * sizeof(int);
-	int *ha, *hb, *hc, *da, *db, *dc;
+	// C = m * n
+	float *hc, *dc;
+	size_t c_size = m * n * sizeof(float);
+
+	float alpha = 2.0;
+
+	hc = (float *)malloc(c_size);
+	cudaMalloc(&dc, c_size);
+
+	// fill matrices
+	fill_matrix(ha, m, k);
+	fill_matrix(hb, k, n);
+
+	cudaMemcpy(da, ha, a_size, cudaMemcpyHostToDevice);
+	cudaMemcpy(db, hb, b_size, cudaMemcpyHostToDevice);
 
 	dim3 threads(16,16);
-	int blocks = (N + 16 - 1) / 16;
-	dim3 gridSize(blocks, blocks);
-
-	ha = (int *) malloc(size);
-	hb = (int *) malloc(size);
-	hc = (int *) malloc(size);
-
-	cudaMalloc(&da, size);
-	cudaMalloc(&db, size);
-	cudaMalloc(&dc, size);
+	int blocksX = (n + threads.x - 1) / threads.x;
+	int blocksY = (m + threads.y - 1) / threads.y;
+	dim3 gridSize(blocksX, blocksY);
+	dim3 gridSize_m(blocksY, blocksX);
 	
-	cudaMemcpy(da, ha, size, cudaMemcpyHostToDevice);
-	cudaMemcpy(db, hb, size, cudaMemcpyHostToDevice);
-	
+	size_t tile_width = 16;
+
 
 	cudaEvent_t start, stop;
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 
 	cudaEventRecord(start);
-	matrixMul<<<gridSize, threads>>>(da, db, dc);
+	gemm_v0<<<gridSize, threads>>>(m, n, k, da, db, dc, alpha); // ~ 9.8 ms
+	//gemm_v1_global<<<gridSize_m, threads>>>(m, n, k, da, db, dc, alpha);
+	//gemm_shared<<<gridSize, threads>>>(m, n, k, tile_width, da, db, dc, alpha);
 	cudaEventRecord(stop);
 
 	cudaEventSynchronize(stop);
 	float ms = 0;
 	cudaEventElapsedTime(&ms, start, stop);
-	printf("Time in kernel: %f on: %d elements\n.", ms, N);
+	printf("Time in kernel: %f on: %zu elements\n.", ms, m * n);
 	
-	cudaMemcpy(hc, dc, size, cudaMemcpyDeviceToHost);
-	check_errors(ha, hb, hc);
-	printf("Succesfully completed BatChest!\n");
+	cudaMemcpy(hc, dc, c_size, cudaMemcpyDeviceToHost);
+	
+	check_errors(m, n, k, alpha, ha, hb, hc);
 	return 0;
 }
--- nn/nn.cu
+++ nn/nn.cu
@@ -69,3 +69,42 @@ __global__ void init_weights(int w, int h, float *out){
 		out[row*w + col] = curand_normal(&state) * sqrtf(2.f/h);
 	}
 }
+
+__global__ void cross_entropy_backwards(int w, int h, float *p, float *r, float *out){
+	int col = threadIdx.x + blockDim.x * blockIdx.x;
+	int row = threadIdx.y + blockDim.y * blockIdx.y;
+
+	if(row < w && col < h){
+		out[row * w + col] = p[row * w + col] - r[row * w + col];
+	}
+}
+
+
+__global__ void backward(int batch_size, int n, int out_w, float * weights, float *biases, float *d_1, float *out_d_1){
+	int col = threadIdx.x + blockDim.x * blockIdx.x;
+	int row = threadIdx.y + blockDim.y * blockIdx.y;
+
+	if(row < batch_size && col < out_w){
+		float d = 0.f;
+		for(int i = 0; i < n; i++){
+			float w = weights[i * out_w + col];
+			d1 += w*d_1[row*n + i];
+		}
+		out_d_1[row * out_w + col] = d1;
+	}
+}
+
+
+__global__ void relu_backwards(int w, int h, float *a, float *d_1, float *b){
+	int col = threadIdx.x + blockDim.x * blockIdx.x;
+	int row = threadIdx.y + blockDim.y * blockIdx.y;
+
+	if(row < h && col < w){
+		float activation = a[row * w + col];
+		if(activation > 0.0f){
+			b[row*w + column] = activation;
+		}else{
+			b[row * w + column] = 0.0f;
+		}
+	}
+}
